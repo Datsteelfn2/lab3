5210d1a1578df30f0745c5e09aa0b6ca1a075c1a
Datsteelfn2 <datsteelfn@gmail.com>
2025-09-17 14:52:39 -0400

    Added calculations for whole pizzas

--- Lab3.py
+++ Lab3.py
@@ -15,3 +15,11 @@ if salads>10:
   print(f"Salad Cost: {salads_total}")
 print(f"Discount: {salads_total*.15}")
 
+#pizza 
+pizza_cost=7.99
+pizza_slices=int(input("How many Pizza Slices:> "))
+if pizza_slices<12:
+  print("You can only order whole pizzas, which are atleast 12 slices")
+elif pizza_slices>=12:
+  whole_pizzas=int((pizza_slices+11)/12)
+  print(whole_pizzas)
