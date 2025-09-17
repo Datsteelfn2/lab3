91bff0bcc24e068035f0de97a0821d5711406b37
Datsteelfn2 <datsteelfn@gmail.com>
2025-09-17 18:42:54 -0400

    Finished program

--- Lab3.py
+++ Lab3.py
@@ -4,22 +4,34 @@
 # Assignment Number: Lab3
 # Due Date: 9/21/ 2025
 # Purpose:The purpose of the program is to calculate the cost of pizza and salads based on inputs and discounts
+delivery=20
+pizza_discount=0
+salads_discount=0
+total_discount=0
 
-#Salads
-
+pizza_slices=int(input("How many Pizza Slices:> "))
 salads=int(input("How many salads:> "))
+#salads
 costOfSalads=7.99
 salads_total=costOfSalads*salads
 discount=.15
-if salads>10:
-  print(f"Salad Cost: {salads_total}")
-print(f"Discount: {salads_total*.15}")
+
 
 #pizza 
-pizza_cost=7.99
-pizza_slices=int(input("How many Pizza Slices:> "))
-if pizza_slices<12:
-  print("You can only order whole pizzas, which are atleast 12 slices")
-elif pizza_slices>=12:
-  whole_pizzas=int((pizza_slices+11)/12)
-  print(whole_pizzas)
+pizza_cost=15.99
+
+whole_pizzas=int((pizza_slices+11)/12)
+pizza_total=pizza_cost*whole_pizzas
+if whole_pizzas>=10: # If theres more than 10 WHOLE pizzas add a discount on pizzas
+  pizza_discount=pizza_total*discount
+if salads>10: # If theres more than 10 salads, add a discount salads
+  salads_discount= salads_total*discount
+total=pizza_total+salads_total
+total_discount=salads_discount+pizza_discount
+print(f"Whole Pizzas: {whole_pizzas}")
+print("Pizza cost: ",pizza_total)
+print(f"Salad Cost: {salads_total}")
+
+print(f"Discount: {total_discount}")
+print(f"Delivery: ",delivery)
+print(f"Total: {total+delivery-total_discount}")
